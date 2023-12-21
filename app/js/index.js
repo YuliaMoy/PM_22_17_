461b7f8464ef3f7559d054e34ab0aa9594901be0
YuliaMoy <145383022+YuliaMoy@users.noreply.github.com>
2023-12-21 13:53:12 +0200

    ready project

--- app/js/index.js
+++ app/js/index.js
@@ -1,8 +1,8 @@
-
 $(function () {
+    var value = 0;
     var user;
    var xhttp = new XMLHttpRequest();
-    xhttp.open('GET', '/src/js/data.json', true);//js/   
+    xhttp.open('GET', 'js/data.json', true);//js/   
 xhttp.send();
 xhttp.onload = function(){
     if (this.status == 200) {
@@ -19,8 +19,7 @@ xhttp.onload = function(){
     }
    
     var addButton = $(".new-button");
-    var allProjects = $(".projects-all");
-    //var deleteButton=$(".delete-project");
+    var allProjects = $(".all-projects");
     var newProject = '<div class="project-row project-row-new"> <div class="percent percent-new">0%</div><div class="caption caption-new"><span>New project</span> </div><div class="delete-project"><button class="delete-button"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-trash" viewBox="0 0 16 16"><path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/><path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/></svg></button></div></div>'
     addButton.click(function () {
         allProjects.prepend(newProject);
@@ -49,27 +48,20 @@ xhttp.onload = function(){
         });
         proj_r_h.css("display", "flex");
 
-        //$(".slide-chevron").css("transform", "rotateX(180deg)","2000");
-    });
-
-    down_b.click(function () {
-        show_u.slideToggle("2000", "linear");
-        show_u.css("display", "flex");
-        //$("slide-chevron").css("transform", "rotateX(180deg)");
     });
 
     $(".button-reload").click(function () {
-        $(".bi-arrow-counterclockwise").css("transform", "rotate(369deg)");
+        value += 50;
+        $(".bi-arrow-counterclockwise").css("transform",`rotate(${360+value}deg)`);
     });
 
-
-    $(".icon-fill-1").mouseover(function () {
-        $(".icon-fill-1").css("fill", "#ff503f");
+    $(".icon-fill1").mouseover(function () {
+        $(".icon-fill1").css("fill", "#ff503f");
         $(".text-icon-1").css("color", "white");
     });
 
-    $(".icon-fill-1").mouseleave(function () {
-        $(".icon-fill-1").css("fill", "#595959");
+    $(".icon-fill1").mouseleave(function () {
+        $(".icon-fill1").css("fill", "#595959");
         $(".text-icon-1").css("color", "#595959");
     });
     $(".icon-fill-2").mouseover(function () {
@@ -111,13 +103,18 @@ xhttp.onload = function(){
         $(".icon-fill-5").css("fill", "#595959");
         $(".text-icon-5").css("color", "#595959");
     });
+
+    down_b.click(function () {
+        show_u.slideToggle("2000", "linear");
+        show_u.css("display", "block");
+    });
+
 });
+
 $(document).mouseup(function(){
-    var allProjects = $(".projects-all");
     var deleteButton=$(".delete-project");
     deleteButton.click(function(){
         console.log(deleteButton.parent());
         deleteButton.parent().remove();
     });
 });
-
